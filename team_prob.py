ed5e122c4be610fad81c859a8fdb174ce0732198
Sergi Flo <sergifs9@gmail.com>
2019-12-21 20:26:20 +0100

    get your players prob to play

---
+++ team_prob.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import players_file
+import predictions
+
+def print_prob():
+  team = players_file.players_from_file()
+  pre = predictions.predictions()
+  for e in team:
+    for player in team[e]:
+      for p_player in pre[e]:
+        if p_player[:-4] in player:
+          print(p_player)
+
+if __name__=='__main__':
+  print_prob()

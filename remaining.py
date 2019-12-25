103d1ae5dbfccd10df87d13180b2a6a27c0f67ec
Sergi Flo <sergifs9@gmail.com>
2019-12-25 19:53:12 +0100

    remaining players to play the weekend

---
+++ remaining.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import time
+import login
+import players_file
+import futbolfantasy
+
+def players_playing():
+  d=login.login()
+  d.find_element_by_xpath('//*[@id="sidebar"]/div/ul/li[1]/a').click()
+  time.sleep(2)
+  #print(players)
+  #print(len(players))
+  res={}
+  for i in range(1,13):
+    p={'Barcelona':[], 'Real Madrid':[], 'Sevilla':[], 'Atlético':[], 'Real Sociedad':[], 'Getafe':[], 'Athletic':[], 'Valencia':[], 'Levante':[], 'Villarreal':[], 'Granada':[], 'Osasuna':[], 'Betis':[], 'Valladolid':[], 'Alavés':[], 'Eibar':[], 'Mallorca':[], 'Celta':[], 'Leganés':[], 'Espanyol':[]}
+    players=d.find_elements_by_class_name('name')
+    name=players[i].text
+    players[i].click()
+    if name == 'sersik9':
+      d.find_element_by_xpath('//*[@id="points-tab"]').click()
+    time.sleep(2)
+    found=False
+    while not found:
+      j=d.find_elements_by_class_name('card-title')
+      ts=d.find_elements_by_xpath('//div[@class="lineUp col-sm-12 text-center animated fadeIn fast"]//span[@class="team-img rotate45"]')
+      if name == 'sersik9':
+        j,ts=j[11:], ts[11:]
+      if name =='DaniPXX':
+        break
+      if len(j)==11 and len(ts)==11:
+        found=True
+    jp=[]
+    for e in j:
+      if e !='':
+        jp.append((e.text).replace('í','i').replace('á','a').replace('é','e').replace('ó','o').replace('ú','u').replace('Í','I').replace('Á','A').replace('É','E').replace('Ó','O').replace('Ú','U').replace('à','a').replace('ć','c'))
+    for e in p:
+      for player,t in zip(jp,ts):
+        if e[-5:].lower().replace('é','e') in t.get_attribute('style'):
+          p[e].append(player)
+    res[name]=p
+    d.find_element_by_xpath('//*[@id="sidebar"]/div/ul/li[1]/a').click()
+    time.sleep(1)
+  #for e in res:
+  print(res)
+  d.close()
+  return res
+
+def players_remaining():
+  res={}
+  ty,tc,tn=futbolfantasy.games_played()
+  #ty,tc=['Eibar', 'Granada', 'Mallorca', 'Sevilla', 'Barcelona', 'Alavés', 'Villarreal', 'Getafe', 'Valladolid', 'Valencia', 'Leganés', 'Espanyol', 'Osasuna', 'Real Sociedad', 'Betis', 'Atlético', 'Levante', 'Celta'], ['Real Madrid', 'Athletic']
+  pp=players_playing()
+  plantillas=players_file.all_marca_players_from_file()
+  for player in pp: #every player playing in the league
+    y,c,n=0,0,0
+    for team in ty: #teams that have already played
+      for e in pp[player]: #e is dic of teams and players from player
+        if e==team: #if team has played
+          for p in pp[player][e]: #p is every player(real) of team of player that has played
+            for a in plantillas[team]: #a is all players in team
+              #print(p.lower(),' | ',a.lower())
+              if p.lower() in a.lower():
+                y+=1
+                break
+    res[player]=[y,11-y]
+  print(res)
+  return res
+
+def print_players_remaining():
+  #d=players_remaining()
+  d={'Maria Pajas': [11, 0], 'Suebrio': [11, 0], 'BayernDeLosCaídos': [10, 1], 'Viniciusbalondeoro': [8, 3], 'sersik9': [9, 2], 'Alvarín': [10, 1], 'Satisfyer 3000': [10, 1], 'Alejo Moudina': [11, 0], 'Guasp': [10, 1], 'Kaifer': [8, 3], 'TomásRoncero07': [10, 1], 'DaniPXX': [8, 3]}
+  print('-'*43)
+  print('Players'.ljust(20),'| Remaining'.ljust(8),'| Played |')
+  print('-'*43)
+  for player in d:
+    print(player.ljust(20),'|    ',str(d[player][1]).ljust(5),'|  ',str(d[player][0]).ljust(4),'|')
+
+if __name__=='__main__':
+  a=input('1-Players remaining, 2- Players playing, 3-Print players remaining ---> ')
+  if a=='1':
+    players_remaining()
+  elif a=='3':
+    print_players_remaining()
+  else:
+    players_playing()

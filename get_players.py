54847b011f3b5a43f02a3d82d26c4a60dbe4baa8
Sergi Flo <sergifs9@gmail.com>
2019-12-25 19:46:01 +0100

    get players from your team

--- get_players.py
+++ get_players.py
@@ -1,44 +1,10 @@
 #!/usr/bin/env python3
 
-import pickle
-import pprint
 import time
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-
-def load_cookies(driver, location, url=None):
-
-    cookies = pickle.load(open(location, "rb"))
-    driver.delete_all_cookies()
-    # have to be on a page before you can add any cookies, any page - does not matter which
-    driver.get("https://google.com" if url is None else url)
-    for cookie in cookies:
-      if 'expiry' in cookie:
-        del cookie['expiry']
-      driver.add_cookie(cookie)
+import login
 
 def get_players():
-  # Path where you want to save/load cookies to/from aka C:\my\fav\directory\cookies.txt
-  cookies_location = "/Users/Sergiflo/Programacion/fantasy/cookies.txt"
-  options=Options()
-  options.add_argument('user-data-dir:/Users/SergiFlo/Programacion/fantasy/chrome')
-  
-  
-  #get to league, success
-  d=webdriver.Chrome(chrome_options=options)
-  load_cookies(d,cookies_location)
-  #time.sleep(1)
-  d.get('https://www.laligafantasymarca.com/login')
-  time.sleep(2)
-  c=d.find_element_by_xpath('/html/body/div[1]/div[1]/div/button')
-  c.click()
-  a=d.find_element_by_xpath('/html/body/fy-app/fy-layout/div/div/div/main/fy-login/div/div/div/div[1]/button/span')
-  a.click()
-  time.sleep(5)
-  d.find_element_by_xpath('/html/body/fy-app/fy-layout/div/div/div/main/fy-home/div/div[1]/div[2]/div/div/fy-league-item/div/div[2]').click()
-  time.sleep(2)
+  d=login.login()
   d.find_element_by_xpath('//*[@id="sidebar"]/div/ul/li[2]/a/span').click()
   time.sleep(3)
   team = d.current_url
@@ -52,25 +18,7 @@ def get_players():
   print(players)
 
 def print_players():
-  # Path where you want to save/load cookies to/from aka C:\my\fav\directory\cookies.txt
-  cookies_location = "/Users/Sergiflo/Programacion/fantasy/cookies.txt"
-  options=Options()
-  options.add_argument('user-data-dir:/Users/SergiFlo/Programacion/fantasy/chrome')
-  
-  
-  #get to league, success
-  d=webdriver.Chrome(chrome_options=options)
-  load_cookies(d,cookies_location)
-  #time.sleep(1)
-  d.get('https://www.laligafantasymarca.com/login')
-  time.sleep(2)
-  c=d.find_element_by_xpath('/html/body/div[1]/div[1]/div/button')
-  c.click()
-  a=d.find_element_by_xpath('/html/body/fy-app/fy-layout/div/div/div/main/fy-login/div/div/div/div[1]/button/span')
-  a.click()
-  time.sleep(5)
-  d.find_element_by_xpath('/html/body/fy-app/fy-layout/div/div/div/main/fy-home/div/div[1]/div[2]/div/div/fy-league-item/div/div[2]').click()
-  time.sleep(2)
+  d=login.login()
   d.find_element_by_xpath('//*[@id="sidebar"]/div/ul/li[2]/a/span').click()
   time.sleep(3)
   team = d.current_url
